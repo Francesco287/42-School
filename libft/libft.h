5fdc848c32e324e0a01ed36b1339a8d203f63ffb
Francesco Gaudio <francescogaudio003@gmail.com>
2024-02-23 19:55:22 +0100

    ft_lstadd_front_bonus

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: fgaudio <fgaudio@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 21:10:54 by fgaudio           #+#    #+#             */
-/*   Updated: 2024/02/23 18:52:38 by fgaudio          ###   ########.fr       */
+/*   Updated: 2024/02/23 19:32:29 by fgaudio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,5 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 #endif
