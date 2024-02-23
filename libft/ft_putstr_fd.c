5981aa4e146f73a4382a2f10dab87d06423f664c
Francesco Gaudio <francescogaudio003@gmail.com>
2024-02-23 16:23:48 +0100

    ft_putstr_fd

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgaudio <fgaudio@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 16:15:06 by fgaudio           #+#    #+#             */
-/*   Updated: 2024/02/23 16:23:03 by fgaudio          ###   ########.fr       */
+/*   Created: 2024/02/23 16:21:15 by fgaudio           #+#    #+#             */
+/*   Updated: 2024/02/23 16:23:21 by fgaudio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, &c, 1);
+	if (!s)
+		return ;
+	while (*s)
+		ft_putchar_fd(*s++, fd);
 }
