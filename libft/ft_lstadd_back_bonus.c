98861214d32084c07f8016b159947eb16adda243
Francesco Gaudio <francescogaudio003@gmail.com>
2024-02-25 00:22:20 +0100

    lstlast lstadd_back

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgaudio <fgaudio@student.42roma.it>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 00:03:31 by fgaudio           #+#    #+#             */
+/*   Updated: 2024/02/25 00:03:31 by fgaudio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst);
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
