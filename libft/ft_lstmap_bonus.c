db14fdbdd68d79a150885424f67ef506249d388a
Francesco Gaudio <francescogaudio003@gmail.com>
2024-02-25 01:15:47 +0100

    lstmap

---
+++ libft/ft_lstmap_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgaudio <fgaudio@student.42roma.it>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 00:56:32 by fgaudio           #+#    #+#             */
+/*   Updated: 2024/02/25 00:56:32 by fgaudio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*elem;
+
+	if (!lst || !f || !del)
+		return NULL;
+	new_list = NULL;
+	while (lst)
+	{
+		elem = ft_lstnew((*f)(lst->content));
+		if (!elem)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, elem);
+		lst = lst->next;
+	}
+	return (new_list);
+}
