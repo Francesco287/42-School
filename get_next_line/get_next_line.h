079f6f50821be8b19185b7e7df50e7f369a42a4a
francesco <francescogaudio003@gmail.com>
2024-04-27 21:22:05 +0200

    seems to work

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,6 @@
 #ifndef GET_NEXT_LINE
 #define GET_NEXT_LINE
 
-
-
-
-
-
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,16 +12,5 @@
 char	*get_next_line(int fd);
 void	add_one(char **str, int i);
 int	get_next_char(int fd, char **buffer, int i);
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,80 @@
+#include "get_next_line.h"
+
+void	str_cpy(char *dest, const char *src, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
+
+void	add_one(char **str, int i)
+{
+	char	*tmp;
+
+	if (i == 0)
+	{
+		*str = malloc(1);
+		return ;
+	}
+	tmp = malloc(i);
+	str_cpy(tmp, *str, i);
+	free(*str);
+	*str = malloc(i + 1);
+	str_cpy(*str, tmp, i);
+	free(tmp);
+}
+
+int	get_next_char(int fd, char **buffer, int i)
+{
+	static void	*s_buf[3];
+
+	if (s_buf[0] == NULL)
+	{
+		s_buf[0] = malloc(BUFFER_SIZE * sizeof(char));
+		s_buf[1] = s_buf[0] + BUFFER_SIZE;
+		s_buf[2] = malloc(1 * sizeof(int));
+	}
+	if (s_buf[1] - s_buf[0] == BUFFER_SIZE)
+	{
+		*((int *)s_buf[2]) = (int)read(fd, (char *)s_buf[0], BUFFER_SIZE);
+		if (*((int *)s_buf[2]) == 0)
+			return (0);
+		s_buf[1] = s_buf[0];
+	}
+	if (s_buf[1] - s_buf[0] == *((int *)s_buf[2]))
+		return (0);
+	add_one(buffer, i);
+	(*buffer)[i] = *((char *)s_buf[1]);
+	s_buf[1]++;
+	return (1);
+}
+
+// int	get_next_char(int fd, char **buffer, int i)
+// {
+// 	static char	*s_buf[2];
+// 	static int	char_read;
+
+// 	if (s_buf[0] == NULL)
+// 	{
+// 		s_buf[0] = malloc(BUFFER_SIZE);
+// 		s_buf[1] = s_buf[0] + BUFFER_SIZE;
+// 	}
+// 	if (s_buf[1] - s_buf[0] == BUFFER_SIZE)
+// 	{
+// 		char_read = read(fd, s_buf[0], BUFFER_SIZE);
+// 		if (char_read == 0)
+// 			return (0);
+// 		s_buf[1] = s_buf[0];
+// 	}
+// 	if (s_buf[1] - s_buf[0] == char_read)
+// 		return (0);
+// 	add_one(buffer, i);
+// 	(*buffer)[i] = *(s_buf[1]);
+// 	s_buf[1]++;
+// 	return (1);
+// }
